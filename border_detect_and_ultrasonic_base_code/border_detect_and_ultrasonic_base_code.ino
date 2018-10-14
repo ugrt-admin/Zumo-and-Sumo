d08cccd343c57bf508c2f197005dda21978978c9
ugrt-admin <44064721+ugrt-admin@users.noreply.github.com>
2018-10-14 06:48:41 -0400

    Created border detect and ultrasonic base code

---
+++ border_detect_and_ultrasonic_base_code/border_detect_and_ultrasonic_base_code.ino
@@ -0,0 +1,131 @@
+
+//import libraries
+#include <Wire.h>
+#include <ZumoShield.h>
+
+#define LED 13
+
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD  1500 // microseconds
+
+// these might need to be tuned for different motor types
+#define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
+#define TURN_SPEED        200
+#define FORWARD_SPEED     200
+#define KILL_SPEED        300 //speed at which zumo will move at when an object is detected
+#define REVERSE_DURATION  200 // ms
+#define TURN_DURATION     300 // ms
+
+//the pins which the ultrasonic sensor connect to
+const int trigPin = 6;
+const int echoPin = 1;
+
+//variables used for distance measurement
+long duration;
+int distance;
+
+//create zumo objects to interface with the zumo using the previously imported library
+ZumoBuzzer buzzer;
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+
+
+#define NUM_SENSORS 6 //6 reflectance sensors, look under the Zumo, you'll see
+unsigned int sensor_values[NUM_SENSORS];
+
+ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
+
+//led on zumo will be on until you press the countdown button
+void waitForButtonAndCountDown()
+{
+  digitalWrite(LED, HIGH);
+  button.waitForButton();
+  digitalWrite(LED, LOW);
+
+  // play audible countdown
+  for (int i = 0; i < 3; i++)
+  {
+    delay(1000);
+    buzzer.playNote(NOTE_G(3), 200, 15);
+  }
+  delay(1000);
+  buzzer.playNote(NOTE_G(4), 500, 15);
+  delay(1000);
+}
+
+void setup()
+{
+
+  // uncomment if necessary to correct motor directions
+  //motors.flipLeftMotor(true);
+  //motors.flipRightMotor(true);
+
+  
+  waitForButtonAndCountDown(); //wait for countdown to start
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+
+  pinMode(LED, HIGH);
+
+}
+
+void loop()
+{
+  if (button.isPressed())
+  {
+    // if button is pressed, stop and wait for another press to go again
+    motors.setSpeeds(0, 0);
+    button.waitForRelease();
+    waitForButtonAndCountDown();
+  }
+
+  //read the values of the reflectance sensors
+  sensors.read(sensor_values);
+
+  if (sensor_values[0] < QTR_THRESHOLD)
+  {
+    // if leftmost sensor detects line, reverse and turn to the right
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else if (sensor_values[5] < QTR_THRESHOLD)
+  {
+    // if rightmost sensor detects line, reverse and turn to the left
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else
+  {
+    // otherwise, go straight
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+
+  if (getDistance() < 20) //if an object is detected within roughly 20cm
+  {
+    motors.setSpeeds(KILL_SPEED, KILL_SPEED); //charge directly in to it, attempting to knock it from the ring
+  }
+
+}
+
+
+double getDistance() //function which returns distance
+{
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  return duration * 0.034 / 2;
+}
